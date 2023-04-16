95d39bd50b98dea0bab153cac29d9cb5044a7d34
ChaeYubin <cyoobin@naver.com>
2023-04-16 20:28:06 +0900

    ✨ register page component event feature

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="auth-wrapper">
       <h1>회원가입.</h1>
       <form>
-        <input placeholder="Apple ID" type="email" name="email" value="" />
-        <input placeholder="암호" type="password" name="password" value="" />
+        <input
+          onChange={handleEmailChange}
+          placeholder="Apple ID"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePasswordChange}
+          placeholder="암호"
+          type="password"
+          name="password"
+          value={password}
+        />
         <button type="submit">회원가입</button>
         <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
           이미 Apple ID가 있다면? 지금 로그인.
